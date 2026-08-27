e5d5f03b90ba0d015d6e651e56166abcbf94b020
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-08-27 10:21:43 +0200

    Adding social_iqa evaluation

--- evaluation/mcqa.py
+++ evaluation/mcqa.py
@@ -211,5 +211,5 @@ if __name__ == "__main__":
         evaluator.pragmega_evaluate(dataset)
     elif args.dataset_name == "allenai/social_i_qa":
         logger.info("MCQA Evaluation on the Social-IQA dataset")
-        print("Sample 1: ", dataset["validation"][0])
+        evaluator.social_iqa_evaluate(dataset["validate"])
         
