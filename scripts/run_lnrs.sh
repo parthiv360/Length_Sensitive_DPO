d8733add038fb444b17ea1030d371bcffe0a2a8f
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-09-04 17:05:53 +0200

    Adding LNRS pipeline

--- scripts/run_lnrs.sh
+++ scripts/run_lnrs.sh
@@ -23,7 +23,7 @@ echo "=========================================="
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
     --model-name "allenai/open-instruct-pythia-6.9b-tulu" \
-    --dataset-name "UCL-DARK/ludwig"
+    --dataset-name "lm-pragmatics"
 
 echo "=========================================="
 echo "MCQA Execution Completed"
