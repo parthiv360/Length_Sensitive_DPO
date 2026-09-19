9a4f57389ed0926202c0360b2fab82571f979902
Parthiv Sarkar <kakarot@asapool2339.vpn.uni-saarland.de>
2026-09-19 15:15:05 +0530

    Adding LS DPO training pipeline

--- scripts/train_sft.sh
+++ scripts/train_sft.sh
@@ -37,7 +37,7 @@ echo "=========================================="
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
     --model_name "meta-llama/Llama-2-13b-chat-hf" \
     --dataset_name "allenai/social_i_qa,cfilt/PUB" \
-    --output_dir "sft_model_output" \
+    --output_dir "sft_model_output/llama-2-13b-chat" \
     --num_train_epochs 1 \
     --batch_size 64 \
     --gradient_accumulation_steps 1 \
