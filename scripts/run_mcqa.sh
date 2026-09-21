0d15285243ad79b1c53cfd0594abdfe2b2114983
Parthiv Sarkar <kakarot@asapool2473.vpn.uni-saarland.de>
2026-09-21 20:33:52 +0530

    eval

--- scripts/run_mcqa.sh
+++ scripts/run_mcqa.sh
@@ -22,7 +22,7 @@ echo "Conda Environment: $CONDA_ENV_NAME"
 echo "=========================================="
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "meta-llama/Llama-2-7b-chat-hf" \
+    --model-name "meta-llama/Llama-2-13b-chat-hf" \
     --dataset-name "UCL-DARK/ludwig"
 
 echo "=========================================="
