a7c6cfe8a4eff82988631755b7294b473e5a71cb
Parthiv Sarkar <kakarot@asapool2414.vpn.uni-saarland.de>
2026-09-22 12:19:12 +0530

    eval

--- scripts/run_mcqa.sh
+++ scripts/run_mcqa.sh
@@ -23,7 +23,7 @@ echo "=========================================="
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
     --model-name "sft_model_output/pythia-6.9b-tulu" \
-    --dataset-name "lm-pragmatics"
+    --dataset-name "allenai/social_i_qa"
 
 echo "=========================================="
 echo "MCQA Execution Completed"
