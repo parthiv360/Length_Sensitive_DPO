aca417bbeb0f5e38348d8a720329b99317a01b35
Parthiv Sarkar <kakarot@asapool2400.vpn.uni-saarland.de>
2026-09-23 11:53:30 +0530

    eval

--- evaluation/lnrs.py
+++ evaluation/lnrs.py
@@ -311,7 +311,7 @@ class LNRSEvaluator:
         return (
             f"Utterance: {data['utterance']}\n"
             f"Response: {data['response']}\n"
-            f"Does the response imply that the answer to the utterance is yes or no?\n"
+            f"Does the response imply that the answer to the utterance is yes or no? Only answer yes or no.\n"
             f"Answer:"
         )
 
