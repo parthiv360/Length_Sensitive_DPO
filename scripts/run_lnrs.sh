9d72f9a6ded1932e7037dc9c7cbee0117a1dc40f
Parthiv Sarkar <kakarot@asapool2263.vpn.uni-saarland.de>
2026-09-24 17:02:58 +0530

    applying chat template to training

--- scripts/run_lnrs.sh
+++ scripts/run_lnrs.sh
@@ -57,7 +57,7 @@ echo "=========================================="
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
     --model-name "meta-llama/Llama-2-13b-chat-hf" \
-    --dataset-name "lm-pragmatics"
+    --dataset-name "UCL-DARK/ludwig"
 
 # "$CONDA_PYTHON" -m "$MODULE_NAME" \
 #     --model-name "dpo_model_output/llama-2-13b-chat" \
