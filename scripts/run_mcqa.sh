ad7986a8cfee72fcce52a667b77a14ee5810b429
Parthiv Sarkar <kakarot@asapool2271.vpn.uni-saarland.de>
2026-09-25 21:18:13 +0530

    Adding ensemble method

--- scripts/run_mcqa.sh
+++ scripts/run_mcqa.sh
@@ -23,41 +23,41 @@ echo "Script: $SCRIPT_NAME"
 echo "Conda Environment: $CONDA_ENV_NAME"
 echo "=========================================="
 
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/pythia-6.9b-tulu" \
-    --dataset-name "allenai/social_i_qa"
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/pythia-6.9b-tulu" \
+#     --dataset-name "allenai/social_i_qa"
 
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/llama-2-7b-chat" \
-    --dataset-name "allenai/social_i_qa"
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/llama-2-7b-chat" \
+#     --dataset-name "allenai/social_i_qa"
 
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/llama-2-13b-chat" \
-    --dataset-name "allenai/social_i_qa"
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/llama-2-13b-chat" \
+#     --dataset-name "allenai/social_i_qa"
 
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/pythia-6.9b-tulu" \
-    --dataset-name "lm-pragmatics"
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/pythia-6.9b-tulu" \
+#     --dataset-name "lm-pragmatics"
 
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/llama-2-7b-chat" \
-    --dataset-name "lm-pragmatics"
-
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/llama-2-13b-chat" \
-    --dataset-name "lm-pragmatics"
-
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/pythia-6.9b-tulu" \
-    --dataset-name "UCL-DARK/ludwig"
-
-"$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "ls_dpo_model_output/llama-2-7b-chat" \
-    --dataset-name "UCL-DARK/ludwig"
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/llama-2-7b-chat" \
+#     --dataset-name "lm-pragmatics"
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
     --model-name "ls_dpo_model_output/llama-2-13b-chat" \
-    --dataset-name "UCL-DARK/ludwig"
+    --dataset-name "lm-pragmatics"
+
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/pythia-6.9b-tulu" \
+#     --dataset-name "UCL-DARK/ludwig"
+
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/llama-2-7b-chat" \
+#     --dataset-name "UCL-DARK/ludwig"
+
+# "$CONDA_PYTHON" -m "$MODULE_NAME" \
+#     --model-name "ls_dpo_model_output/llama-2-13b-chat" \
+#     --dataset-name "UCL-DARK/ludwig"
 
 
 
