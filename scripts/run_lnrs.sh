b33b444bc676b74f0b0f744ebd1f52e11622bbe3
Parthiv Sarkar <kakarot@asapool2254.vpn.uni-saarland.de>
2026-09-26 15:13:31 +0530

    Adding ensemble method

--- scripts/run_lnrs.sh
+++ scripts/run_lnrs.sh
@@ -74,8 +74,8 @@ echo "=========================================="
 #     --dataset-name "allenai/social_i_qa"
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --model-name "sft_model_output/llama-2-13b-chat" \
-    --dataset-name "UCL-DARK/ludwig"
+    --model-name "ls_dpo_model_output/llama-2-13b-chat" \
+    --dataset-name "allenai/social_i_qa"
 
 # "$CONDA_PYTHON" -m "$MODULE_NAME" \
 #     --model-name "sft_model_output/llama-2-13b-chat" \
